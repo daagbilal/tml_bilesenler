8188f76615a9c4c6151de281b3e3b76b20020464
Bilal DAĞ <daagbilal@hotmail.com>
2023-12-29 16:08:43 +0300

    commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+
+  const handleclick = () => {
+    alert("Merhaba")
+  };
+
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Click Button' onPress={handleclick} />
+
+      <TouchableOpacity onPress={handleclick}>
+        <Text>Click TouchableOpacity</Text>
+      </TouchableOpacity>
+
+      <TouchableHighlight
+        activeOpacity={0.8}
+        underlayColor="yellow"
+        onPress={() => alert('Pressed!')}>
+        <Text>Click TouchableHighlight</Text>
+      </TouchableHighlight>
+
+      <TouchableWithoutFeedback onPress={() => alert('Pressed!')}>
+        <Text>Click TouchableWithoutFeedback</Text>
+      </TouchableWithoutFeedback>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInputChange}
+        value={inputText}
+        placeholder="Enter text..."
+      />
     </View>
   );
 }
@@ -17,4 +48,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
 });
+
